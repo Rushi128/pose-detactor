8c27c84e51085ca230154ab8fabd50f46fdf3388
Rushi Thorat <89934065+Rushi128@users.noreply.github.com>
2022-08-26 22:28:26 +0530

    Add files via upload

---
+++ pose_detector.py
@@ -0,0 +1,45 @@
+import cv2
+import mediapipe as mp
+import time
+# import posemodule
+# from cvzone.HandTrackingModule import HandDetector
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture(1)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 130)
+pTime = 0
+
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    # print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv2.circle(img, (cx, cy), 5, (0, 255, 0), cv2.FILLED)
+
+
+
+
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (100, 150),
+                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("video", img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
